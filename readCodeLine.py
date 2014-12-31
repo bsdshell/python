77a6d44c7d2f13ca76a016dd2d156c5cf6c72039
root <root@worlddog.(none)>
2014-12-30 19:14:08 -0500

    do it

--- readCodeLine.py
+++ readCodeLine.py
@@ -3,43 +3,32 @@ import os;
 import sys;
 import subprocess
 
-readHandler   = open('/tmp/text.txt')
-fileDir = "/tmp/"
+
+# python readCodeLine.py file.java  1   10
+
 arglist = sys.argv;
 
-count = 0
 index0 = 0
 index1 = 0
         
-basename = os.path.basename("/dog/dog/dog.txt")
-pair = os.path.splitext(basename)
-newFileName = fileDir + pair[0] + ".html"
-writeHandler  = open(newFileName, "w+")
-currDir = os.getcwd();
+currDir = os.getcwd()
 
-print "currDir="+currDir
-print "newFileName="+newFileName
-print "basename="+basename
-print "noextension="+pair[0]
-print "index0="+str(index0)
-print "index1="+str(index1)
-for line in iter(readHandler):
-        if len(arglist) == 2 and arglist[1] == "l":
-            print "["+str(count)+"]"+line
-        elif len(arglist) == 4:
-            fileName = arglist[1]
-            fullPath = currDir+"/"+fileName
-            index0 = int(arglist[2])
-            index1 = int(arglist[3])
-            subprocess.call(["pygmentize"])
+# List file line by line
+# readCodeLine file.java l
 
-            print "fullPath="+fullPath
-            print "[1]"+arglist[1]
-            print "[2]"+arglist[2]
-            print "[3]"+arglist[3]
-        if (index0 <= count) and (count <= index1) :
-            print "["+str(count)+line
-            writeHandler.write(line);
-        count = count+1
+if len(arglist) == 3 and arglist[2] == "l":
+    fileName = arglist[1]
+    fullPathFile = currDir + "/" + fileName;
+    print "fullPathFile="+fullPathFile
+    try:
+        readHandler = open(fullPathFile, "r")
+        count = 0
+        for line in iter(readHandler):
+            print "["+str(count)+"]"+line.rstrip("\n")
+            count = count + 1
+    except IOError:
+        print "Open file ["+fullPathFile+"] error"
+
+
 readHandler.close()
-writeHandler.close()
+
