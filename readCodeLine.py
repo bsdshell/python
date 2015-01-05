bf07913a9879dadd3f6dff52310b8ff459141993
root <root@worlddog.(none)>
2015-01-04 19:15:21 -0500

    Update readCodeLine.py

--- readCodeLine.py
+++ readCodeLine.py
@@ -4,24 +4,44 @@ import sys;
 import subprocess
 import inspect
 
-
 # python readCodeLine.py file.java  1   10
+# List file line by line
+# Syntax: readCodeLine file.java l
 
 arglist = sys.argv;
 currDir = os.getcwd()
 index0 = 0
 index1 = 0
 
-# List file line by line
-# Syntax: readCodeLine file.java l
-
-print arglist[0]
-print arglist[1]
-print arglist[2]
 print "currDir="+currDir 
-print __file__ 
-print inspect.currentframe()
-if len(arglist) == 3 and arglist[2] == "l":
+
+def generate(sourceFile, destFile, index0, index1):
+    pair = os.path.splitext(sourceFile)
+    tmpFileName = currDir + "/" + pair[0] + "_tmp.java"
+
+    sourceFullFile = currDir + "/" + sourceFile
+    destFullFile   = currDir + "/" + destFile
+
+    tmpWriteHandler = open(tmpFileName, "w+")
+    readHandler = open(sourceFullFile, "r")
+    count = 0
+    for line in iter(readHandler):
+        if (index0 <= count) and (count <= index1) :
+            print "[" + str(count) +"]" + line.rstrip("\n")
+            tmpWriteHandler.write(line)
+        count = count + 1
+
+    tmpWriteHandler.close()
+    readHandler.close()
+
+    htmlFile = destFullFile 
+    subprocess.call(["pygmentize", "-f", "html", "-o", htmlFile, tmpFileName])
+
+
+if len(arglist) == 1:
+    print "[0] readCodeLine file.java l   [list line #]"
+    print "[1] readCodeLine file.java 1 20 [ Color line 1 to 20, file.html is generated]"
+elif len(arglist) == 3 and arglist[2] == "l":
     fileName = arglist[1]
     fullPathFile = currDir + "/" + fileName;
     print "fullPathFile="+fullPathFile
@@ -39,7 +59,7 @@ if len(arglist) == 3 and arglist[2] == "l":
 
 # Colour line from 1 to 10
 # Syntax: readCodeLine file.java 1 10
-if len(arglist) == 4 and arglist[2].isdigit() and arglist[3].isdigit():
+elif len(arglist) == 4 and arglist[2].isdigit() and arglist[3].isdigit():
     # etc. file.java
     fileName = arglist[1] 
     pair = os.path.splitext(fileName)
@@ -66,3 +86,9 @@ if len(arglist) == 4 and arglist[2].isdigit() and arglist[3].isdigit():
     htmlFile = currDir + "/" + htmlPair[0] + ".html"
     subprocess.call(["pygmentize", "-f", "html", "-o", htmlFile, tmpFileName])
 
+elif len(arglist) == 5 and arglist[3].isdigit() and arglist[4].isdigit():
+    sourceFile = arglist[1]
+    destFile = arglist[2]
+    index0 = int(arglist[3])
+    index1 = int(arglist[4])
+    generate(sourceFile, destFile, index0, index1)
