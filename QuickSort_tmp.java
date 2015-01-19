c960b8bac8b03d007f6f815d02e2856a37f5d2c7
bsdshell <000b000@gmail.com>
2015-01-19 14:11:51 -0800

    Fixed some minor issue in readCodeLine.py

---
+++ QuickSort_tmp.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+//quick sort algorithm
+class QuickSort 
+{
+	public static void main(String args[])
+	{
+        Test1();
+        Test2();
+        Test3();
--- readCodeLine.py
+++ readCodeLine.py
@@ -4,9 +4,8 @@ import sys;
 import subprocess
 import inspect
 
-# python readCodeLine.py file.java  1   10
-# List file line by line
-# Syntax: readCodeLine file.java l
+#The python script generates colour hightlighting code from source code files
+#Use pygmentize to color source code in many programming languages
 
 arglist = sys.argv;
 currDir = os.getcwd()
@@ -15,15 +14,16 @@ index1 = 0
 
 print "currDir="+currDir 
 
-def generate(sourceFile, destFile, index0, index1):
-    pair = os.path.splitext(sourceFile)
+def generate(sourceFile, htmlFile, index0, index1):
+    basename = os.path.basename(sourceFile)
+    pair = os.path.splitext(basename)
     tmpFileName = currDir + "/" + pair[0] + "_tmp.java"
+    print "tmpFileName="+tmpFileName
+    print "sourceFile="+sourceFile
 
-    sourceFullFile = currDir + "/" + sourceFile
-    destFullFile   = currDir + "/" + destFile
 
     tmpWriteHandler = open(tmpFileName, "w+")
-    readHandler = open(sourceFullFile, "r")
+    readHandler = open(sourceFile, "r")
     count = 0
     for line in iter(readHandler):
         if (index0 <= count) and (count <= index1) :
@@ -33,15 +33,13 @@ def generate(sourceFile, destFile, index0, index1):
 
     tmpWriteHandler.close()
     readHandler.close()
-
-    htmlFile = destFullFile 
     subprocess.call(["pygmentize", "-f", "html", "-o", htmlFile, tmpFileName])
 
-
 if len(arglist) == 1:
-    print "[0] readCodeLine file.java l   [list line #]"
-    print "[1] readCodeLine file.java 1 20 [ Color line 1 to 20, file.html is generated]"
-elif len(arglist) == 3 and arglist[2] == "l":
+    print "[0] readCodeLine file.java -l                    [List file.java with line #]"
+    print "[1] readCodeLine file.java 2 20                  [Color line from 2 to 20, file.html is generated in current directory]"
+    print "[2] readCodeLine file.java diffFile.html 2 20    [Color line from 2 to 20, diffFile.html is generated in current directory]"
+elif len(arglist) == 3 and arglist[2] == "-l":
     fileName = arglist[1]
     fullPathFile = currDir + "/" + fileName;
     print "fullPathFile="+fullPathFile
@@ -58,37 +56,25 @@ elif len(arglist) == 3 and arglist[2] == "l":
 
 
 # Colour line from 1 to 10
-# Syntax: readCodeLine file.java 1 10
+# Syntax: readCodeLine source.java 1 10
 elif len(arglist) == 4 and arglist[2].isdigit() and arglist[3].isdigit():
     # etc. file.java
-    fileName = arglist[1] 
-    pair = os.path.splitext(fileName)
-    print currDir
-    tmpFileName = currDir + "/" + pair[0] + "_tmp.java"
-    fullPathFile = currDir + "/" + fileName;
-    tmpWriteHandler = open(tmpFileName, "w+")
-    readHandler = open(fullPathFile, "r")
-    index0 = int(arglist[2])
-    index1 = int(arglist[3])
-    count = 0
-    for line in iter(readHandler):
-        if (index0 <= count) and (count <= index1) :
-            print "[" + str(count) +"]" + line.rstrip("\n")
-            tmpWriteHandler.write(line)
-        count = count + 1
+    basename = os.path.basename(arglist[1])
+    sourceFile = currDir + "/" + basename 
+    fileExt = os.path.splitext(basename)
 
-    tmpWriteHandler.close()
-    readHandler.close()
+    htmlFile = currDir + "/" + fileExt[0] + ".html"
+    print "htmlfile="+htmlFile
+    lineStart= int(arglist[2])
+    lineEnd= int(arglist[3])
 
-    basename = os.path.basename(fileName)
-    htmlPair = os.path.splitext(basename)
+    generate(sourceFile, htmlFile, lineStart, lineEnd)
 
-    htmlFile = currDir + "/" + htmlPair[0] + ".html"
-    subprocess.call(["pygmentize", "-f", "html", "-o", htmlFile, tmpFileName])
-
+# Colour line from 1 to 10
+# Syntax: readCodeLine source.java diffFile.html 1 10
 elif len(arglist) == 5 and arglist[3].isdigit() and arglist[4].isdigit():
     sourceFile = arglist[1]
-    destFile = arglist[2]
-    index0 = int(arglist[3])
-    index1 = int(arglist[4])
-    generate(sourceFile, destFile, index0, index1)
+    htmlFile = arglist[2]
+    lineStart= int(arglist[3])
+    lineEnd= int(arglist[4])
+    generate(sourceFile, htmlFile, lineStart, lineEnd)
